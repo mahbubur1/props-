f1bf331f55f4cd7bfaf3164bc21c1ddf010976eb
mahbubur1 <mdmahbuborr462@gail.com>
2022-04-01 14:33:24 +0600

    props & dynamicly set component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header></Header>
+      {
+        hafejs.map (hafej =><Hafejs name={hafej}></Hafejs> )
+      }
     </div>
   );
 }
+const hafejs = 
+  ["H.M Mojibor Rahman","H.M Mahbub","H.M Mokhlis","H.M Mahdi","H.M Mosfik","H.Ms Mahbuba"];
+
+  function Header (){
+    return (
+  <h1> All Hafeje Quran </h1>
+    )
+  }
+
+function Hafejs (props) {
+  return (
+   <div className='Hafejs-Container'>
+  <h3>{props.name}</h3>
+  </div>
+  )
+};
 
 export default App;
